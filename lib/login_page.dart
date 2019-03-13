c18322ae30da59b47f31ab2fc80d3c28d380bff4
guillaume <guillaume@2denker.de>
2019-03-13 09:53:22 +0100

    trying sending out Yo

---
+++ lib/login_page.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
+import 'package:yo/session_model.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool _isLoading = false;
+
+  Widget _buildSignInButton() {
+    return MaterialButton(
+        child: Center(
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Icon(Icons.person),
+              SizedBox(width: 8),
+              Text("Login with Google"),
+            ],
+          ),
+        ),
+        onPressed: _onSignInClicked);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Color(0xFF6C5B7B),
+        body: Container(
+          child: Center(
+            child: Text("Yo!",
+                style: Theme.of(context)
+                    .textTheme
+                    .display4
+                    .copyWith(fontWeight: FontWeight.bold)),
+          ),
+        ),
+        bottomNavigationBar: SizedBox(
+          height: 64,
+          child: Center(
+            child:
+                _isLoading ? CircularProgressIndicator() : _buildSignInButton(),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _onSignInClicked() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    try {
+      await ScopedModel.of<SessionModel>(context).googleLogin();
+    } catch (e) {
+      print("Error logging in $e");
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
+import 'package:yo/friends_model.dart';
+import 'package:yo/login_page.dart';
+import 'package:yo/session_model.dart';
 import 'person.dart';
 
 Future<void> main() async {
-  runApp(YoApp());
+  final SessionModel sessionModel = SessionModel();
+  runApp(ScopedModel(model: sessionModel, child: YoApp()));
 }
 
 class YoApp extends StatelessWidget {
@@ -14,67 +19,101 @@ class YoApp extends StatelessWidget {
           brightness: Brightness.dark,
           accentColor: Color(0xFFF67280),
         ),
-        home: Scaffold(
-          body: FriendList(),
-        ));
+        home: ScopedModelDescendant<SessionModel>(
+          builder: (BuildContext context, Widget child, SessionModel model) {
+            if (!model.initialized) {
+              // kind of splash screen, before we know if the user is signed in or not
+              return Splash();
+            }
+
+            if (model.isUserLoggedIn) {
+              // still signed in, show home
+              return ScopedModel(
+                  model: FriendsModel(model),
+                  child: FriendsPage()
+                );
+            }
+
+            // new user, show login
+            return LoginPage();
+          },
+      )
+  );
+}
+}
+
+class Splash extends StatelessWidget {
+  const Splash({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Color(0xFF6C5B7B),
+      child: Center(
+        child: SizedBox(
+          width: 48,
+          height: 48,
+          child: CircularProgressIndicator(),
+        ),
+      ),
+    );
   }
 }
 
-class FriendList extends StatelessWidget {
+class FriendsPage extends StatelessWidget {
 
-  final List<Person> friends = [
-    Person("ffff", "Frederik Schweiger", "https://pbs.twimg.com/profile_images/1074391975820972033/SP7txc1D_400x400.jpg"),
-    Person("pppp", "Pascal Welsch", "https://pbs.twimg.com/profile_images/941273826557677568/wCBwklPP_400x400.jpg"),
-    Person("gggg", "Georg Bednorz", "https://pbs.twimg.com/profile_images/1091439933716381701/PIfcpdHq_400x400.png"),
-    Person("ssss", "Seth Ladd", "https://pbs.twimg.com/profile_images/986316447293952000/oZWVUWDs_400x400.jpg"),
-    Person("kkkk", "Kate Lovett", "https://pbs.twimg.com/profile_images/1048927764156432384/JxEqQ9dX_400x400.jpg"),
-    Person("tttt", "Tim Sneath", "https://pbs.twimg.com/profile_images/653618067084218368/XlQA-oRl_400x400.jpg"),
-    Person("hhhh", "Filip Hráček", "https://pbs.twimg.com/profile_images/796079953079111680/ymD9DY5g_400x400.jpg"),
-    Person("aaaa", "Andrew Brogdon", "https://pbs.twimg.com/profile_images/651444930884186112/9vlhNFlu_400x400.png"),
-    Person("nnnn", "Nitya Narasimhan", "https://pbs.twimg.com/profile_images/988808912504733697/z03gHVFL_400x400.jpg"),
-];
-
-List<Color> _colors = [
-    Color(0xFFF8B195),
-    Color(0xFFF67280),
-    Color(0xFFC06C84),
-    Color(0xFF6C5B7B),
-    Color(0xFF355C7D),
-    Color(0xFF34495D),
-];
+  final List<Color> _colors = [
+      Color(0xFFF8B195),
+      Color(0xFFF67280),
+      Color(0xFFC06C84),
+      Color(0xFF6C5B7B),
+      Color(0xFF355C7D),
+      Color(0xFF34495D),
+  ];
 
   Widget build(BuildContext context) {
-    return ListView.builder(
-      padding: EdgeInsets.only(top: 48.0),
-      itemCount: friends.length,
-      itemBuilder: (BuildContext context, int index) {
-        return Container(
-          height: 128,
-          color: _colors[index % _colors.length],
-          child: Row(
-            mainAxisSize: MainAxisSize.max,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: ClipOval(
-                    child: Image.network(
-                      friends[index].photoUrl,
-                      height: 48,
-                      width: 48,
+    return ScopedModelDescendant<FriendsModel>(
+          builder: (BuildContext context, Widget child, FriendsModel model) {
+          return ListView.builder(
+        padding: EdgeInsets.only(top: 48.0),
+        itemCount: model.friends.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Material(
+                      child: InkWell(
+              onTap: () {
+                FriendsModel.of(context).sendYo(model.friends[index]);
+              },
+                        child: Container(
+                height: 128,
+                color: _colors[index % _colors.length],
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: ClipOval(
+                          child: Image.network(
+                            model.friends[index].photoUrl,
+                            height: 48,
+                            width: 48,
+                          ),
+                      ),
                     ),
+                    Text(
+                      model.friends[index].name.toUpperCase(),
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 24
+                      ),
+                      ),
+                  ],
                 ),
               ),
-              Text(
-                friends[index].name.toUpperCase(),
-                style: TextStyle(
-                  fontWeight: FontWeight.w500,
-                  fontSize: 24
-                ),
-                ),
-            ],
-          ),
-        );
-      },
+            ),
+          );
+        },
+      );
+          },
     );
   }
 }
